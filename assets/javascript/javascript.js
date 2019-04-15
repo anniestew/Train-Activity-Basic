a167c3242d9ffcfa8f05fea440cd1fa75fe52c82
Annie Stewart <anniestewart@Annies-MacBook-Air.local>
2019-04-15 12:40:19 -0400

    index

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -9,9 +9,9 @@ var nameEntered = "";
 $("#submit").on("click", function(event) {
     event.preventDefault();
     
-    destinationEntered = $("#position-input").val().trim();
-    frequencyTrain = $("#start-input").val().trim();
-    nextArrival = $("#rate-input").val().trim();
+    destinationEntered = $("#destination-input").val().trim();
+    frequencyTrain = $("#frequency-input").val().trim();
+    nextArrival = $("#arrival-input").val().trim();
     nameEntered = $("#name-input").val().trim();
 
 
@@ -37,16 +37,16 @@ newEntry.on("child_added", function(childSnapshot) {
     var tBody = $("tbody")
     var tRow = $("<tr>");
 
-    // var nameTd = $("<td>").text(newPost.name);
-    // var destinationTd = $("<td>").text(newPost.position);
-    // var monthsTd = $("<td>").text("");
-    // var startTd = $("<td>").text(newPost.startDate);
-    // var rateTd = $("<td>").text(newPost.rate);
-    // var overallTd = $("<td>").text("");
+    var nameTd = $("<td>").text(newPost.name);
+    var destinationTd = $("<td>").text(newPost.desination);
+    var minutesTd = $("<td>").text("");
+    var arrivalTd = $("<td>").text(newPost.arrival);
+    var frequencyTd = $("<td>").text(newPost.frequency);
+    var overallTd = $("<td>").text("");
 
 
-    // tRow.append(nameTd, positionTd, startTd, monthsTd, rateTd, overallTd);
-    // tBody.append(tRow);
-    // $("#table").append(tBody);
+    tRow.append(nameTd, destinationTd, minutesTd, arrivalTd, frequencyTd, overallTd);
+    tBody.append(tRow);
+    $("#table").append(tBody);
 
 });
